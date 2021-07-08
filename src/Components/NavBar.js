ff9d52f178d9667a7e892311c4142da84254c58e
treatme030 <treatme8192@gmail.com>
2021-07-09 00:37:13 +0900

    HomePage에 아이콘 추가

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import avatar from '../img/avatar.png';
+import avatar from '../img/git-img.jpg';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { faFacebook, faGithub, faYoutube} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className="HomePage">
+            <header className="hero">
+                <h1 className="hero-text">
+                    Hi, I am 
+                    <span>Juhee</span>
+                </h1>
+                <p className="h-sub-text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Maiores, eos veritatis? Dolore architecto placeat quidem 
+                    corrupti temporibus aliquid perspiciatis mollitia, 
+                    sed accusantium eveniet culpa repellat blanditiis 
+                    consequuntur atque obcaecati animi.
+                </p>
+                <div className="icons">
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faFacebook} className="icon facebook"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faGithub} className="icon github"/>
+                    </Link>
+                    <Link className="icon-holder">
+                        <FontAwesomeIcon icon={faYoutube} className="icon youtube"/>
+                    </Link>
+                </div>
+            </header>
         </div>
     );
 };
