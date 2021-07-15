0955f948ad3ffff263fab1ebad2849c585f284e5
treatme030 <treatme8192@gmail.com>
2021-07-15 11:58:37 +0900

    Blogs 페이지 추가

---
+++ src/Components/allBlogs.js
@@ -0,0 +1,80 @@
+import blog1 from '../img/blogs/blog1.svg';
+import blog2 from '../img/blogs/blog2.svg';
+import blog3 from '../img/blogs/blog3.svg';
+import blog4 from '../img/blogs/blog4.svg';
+import blog5 from '../img/blogs/blog5.svg';
+
+export default [
+    {
+        id:1,
+        image: blog1,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '21',
+        month: 'Mar',
+        year: '2020'
+    },
+    {
+        id:2,
+        image: blog2,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '09',
+        month: 'Aug',
+        year: '2020'
+    },
+    {
+        id:3,
+        image: blog3,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '28',
+        month: 'Feb',
+        year: '2020'
+    },
+    {
+        id:4,
+        image: blog4,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '16',
+        month: 'May',
+        year: '2021'
+    },
+    {
+        id:5,
+        image: blog5,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '21',
+        month: 'Jun',
+        year: '2021'
+    },
+    {
+        id:6,
+        image: blog3,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '21',
+        month: 'Jun',
+        year: '2021'
+    },
+    {
+        id:7,
+        image: blog2,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '21',
+        month: 'Jun',
+        year: '2021'
+    },
+    {
+        id:8,
+        image: blog1,
+        title: 'UI/UX Design Starter with Figma',
+        link: 'http://www.facebook.com',
+        date: '21',
+        month: 'Jun',
+        year: '2021'
+    }
+];
--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import allBlogs from '../Components/allBlogs';
+import Title from '../Components/Title';
 
 const BlogsPage = () => {
     return (
-        <div>
-            <h1>Blogs Page</h1>
+        <div className="BlogsPage">
+            <Title title={'Recent Blogs'} span={'Recent Blogs'}/>
+            <div className="bolgs-container">
+                {
+                    allBlogs.map(blog => {
+                        return <div key={blog.id} className="blog">
+                                    <div className="blog-content">
+                                        <img src={blog.image} alt="" />
+                                        <a href={blog.link} className="blog-link">
+                                            {blog.title}
+                                        </a>
+                                    </div>
+                                </div>
+                    })
+                }
+            </div>
         </div>
     );
 };
